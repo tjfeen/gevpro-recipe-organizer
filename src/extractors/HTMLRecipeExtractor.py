3110cd95248827386b4f3342661fea15c08d1177
Tjalling Feenstra <hello@tjalling.net>
2025-03-12 08:13:03 +0100

    fix: make get_steps() method work

--- src/extractors/HTMLRecipeExtractor.py
+++ src/extractors/HTMLRecipeExtractor.py
@@ -7,18 +7,33 @@ import json
 class HTMLRecipeExtractor(RecipeExtractor):
     def __init__(self, html_doc):
         self.soup = BeautifulSoup(html_doc, 'html.parser')
-
-    def _get_steps_json(self):
-        """Get recipe steps from the JSON data on the page."""
-        json_tag = self.soup.find('script', type='application/ld+json')
-        if not json_tag: return
         
-        recipe_data = json.loads(json_tag.text)
-        print(recipe_data)
-
+        # Extract recipe linking data
+        ld_tag = self.soup.find('script', type='application/ld+json', string=re.compile(r'"@type":\s*"Recipe"'))
+        self.ld = json.loads(ld_tag.text) if ld_tag else None
+    
+    def get_name(self):
+        if(self.ld['name']): return self.ld['name']
+        
     def get_steps(self):
-        json_steps = self._get_steps_json()
-        number_tags = self.soup.find_all(string=re.compile(r'[0-9]'))
+        steps = self.ld_get_steps()
+        if steps: return steps
+
+    def ld_get_steps(self):
+        """Extract how-to steps from `self.ld`."""
+        if(not self.ld): return
+        
+        steps = None
+        for key in self.ld:
+            value = self.ld[key]
+            if(type(value) is not list): continue
+            if(type(value[0]) is not dict): continue
+            if(value[0]['@type'] != 'HowToStep'): continue
+            
+            steps = value
+        if(not steps): return
+            
+        return list(step['text'] for step in steps)
 
 def main():
     parser = argparse.ArgumentParser()
@@ -28,6 +43,10 @@ def main():
     with open(args.filename, encoding='utf8') as file:
         html_doc = file.read()
         extractor = HTMLRecipeExtractor(html_doc)
+        
+        print('Name: ', end='')
+        print(extractor.get_name())
+        print()
         
         print('Steps:')
         print(extractor.get_steps())
