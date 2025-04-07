7cc671e0671c6add7892ee49fb18588d20653b67
Tjalling Feenstra <hello@tjalling.net>
2025-04-07 10:19:06 +0200

    add RecipeStep class

--- src/RecipeStep.py
+++ src/RecipeStep.py
@@ -1,14 +1,7 @@
 class RecipeStep:
-    def __init__(self, text):
+    def __init__(self, recipe, text):
+        self.recipe = recipe
         self.text = text
-        
-    @staticmethod
-    def from_str(str):
-        return RecipeStep(str.strip())
-    
-    @staticmethod
-    def from_linked_data(ld_step):
-        return RecipeStep(ld_step['text'].strip())
         
     def get_text(self):
         return self.text
